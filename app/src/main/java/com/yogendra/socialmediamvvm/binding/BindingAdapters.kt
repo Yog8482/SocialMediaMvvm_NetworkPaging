a7dae3ff52f064bec75a530a404b46ed2435acf4
YOGENDRA <myog33@gmail.com>
2020-07-17 17:09:24 +0530

    new color for url, binding adapter

---
+++ app/src/main/java/com/yogendra/socialmediamvvm/binding/BindingAdapters.kt
@@ -0,0 +1,32 @@
+package com.elifox.legocatalog.binding
+
+import android.text.method.LinkMovementMethod
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.text.HtmlCompat
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+@BindingAdapter("isGone")
+fun bindIsGone(view: View, isGone: Boolean) {
+    view.visibility = if (isGone) {
+        View.GONE
+    } else {
+        View.VISIBLE
+    }
+}
+
+@BindingAdapter("imageFromUrl")
+fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
+    if (!imageUrl.isNullOrEmpty()) {
+        Glide.with(view.context)
+                .load(imageUrl)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(view)
+    }
+}
+
+
